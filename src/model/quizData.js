c59468718b1e9306170b86adf4d925de067c075b
Swyamhp <swayamgd0@gmail.com>
2025-03-04 12:16:26 +0530

    after delete dist

--- src/model/quizData.js
+++ src/model/quizData.js
@@ -4,6 +4,7 @@ export const getFormData = function(event){
     event.preventDefault();
     model.formData=null;
     const formData = new FormData(event.target);
+    console.log(formData);
     const data =  Object.fromEntries(formData.entries());
     Object.assign(model.formData, data);
     const stringifyFormData = JSON.stringify(data);
