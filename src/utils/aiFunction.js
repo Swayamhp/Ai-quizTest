6e6bab3dc186b323fd3e5966f97f0eec7aba5f79
Swayam Goud <swayamgd0@gmail.com>
2025-12-23 17:00:39 +0530

    added new key

--- src/utils/aiFunction.js
+++ src/utils/aiFunction.js
@@ -1,34 +1,25 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.10.111/pdf.worker.min.js";
 import * as MODEL from '../model/model.js';
 
-const apiKey = "AIzaSyB4RJ8ioY0j2wDG4iSB0ZV3i8AhF3TMkWo"; // Replace with your API key
-const genAI = new GoogleGenerativeAI(apiKey);
+const apiKey = process.env.PARCEL_API_KEY; // Replace with your API key
+const ai = new GoogleGenAI({apiKey:apiKey});
 
 export async function generateQuestions(insertText) {
   try{
-
-  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-  const chatSession = model.startChat({
-    generationConfig: {
-      temperature: 1,
-      topP: 0.95,
-      topK: 40,
-      maxOutputTokens: 8192,
-      responseMimeType: "text/plain",
-    },
+const result = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: insertText,
   });
+  
   if(!insertText){
     throw new Error('Enter a valid topic');
    }
-  const result = await chatSession.sendMessage(
-    `${insertText}`
-  );
- 
- console.log(result);
 
-    const data = result.response.text().slice(7,-4);
+ 
+ console.log(result.text);
+
+    const data = result.text.slice(7,-4);
 
 const finalData = JSON.parse(data);
   localStorage.removeItem('question');
@@ -41,7 +32,7 @@ const finalData = JSON.parse(data);
 
   }catch(err){
 
-    window.alert("Enter a valid topic Or Pdf");
+    console.log(err.message)
   }
   
   
